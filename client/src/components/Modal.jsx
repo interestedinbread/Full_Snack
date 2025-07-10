096f970277288cdeb85fa1178099240d82a52ad1
Eric Morrison <eric_m250@outlook.com>
2025-07-10 11:01:27 -0700

    about to try reconfiguring modal setup

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -5,7 +5,6 @@ export function Modal(props) {
     return ReactDom.createPortal(
         <div className="fixed inset-0 z-10">
             <button className="fixed inset-0 z-20 bg-black bg-opacity-50" onClick={() => {
-                console.log("modal buttn clicked")
                 setSelectedMeal(null)
             }}/>
             <div className="fixed inset-0 w-4/5 mx-auto mt-24 z-50 max-h-[60vh]">
