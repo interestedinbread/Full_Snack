df22faf9a8ddc68def46a5c6c938aa818fc0be07
Eric Morrison <eric_m250@outlook.com>
2025-07-17 09:35:23 -0700

    set up authcontext with use effect to check for user obj in localstorage

--- client/src/components/context/AuthContext.jsx
+++ client/src/components/context/AuthContext.jsx
@@ -1,10 +1,17 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext()
 
 export function AuthProvider({ children }) {
     const [authenticated, setAuthenticated] = useState(false)
     const [username, setUsername] = useState('')
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem('user')
+        if(storedUser){
+            setAuthenticated(true)
+        }
+    }, [])
 
     return (
         <AuthContext.Provider value={{ authenticated, setAuthenticated, username, setUsername }} >
