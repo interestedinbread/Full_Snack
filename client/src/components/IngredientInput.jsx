3b933d753d9260b3eb12b64cfdf12fde46724120
Eric Morrison <eric_m250@outlook.com>
2025-08-05 17:17:30 -0700

    testing animation tweak on buttons

--- client/src/components/IngredientInput.jsx
+++ client/src/components/IngredientInput.jsx
@@ -58,7 +58,7 @@ export function IngredientInput(props) {
                     }
                 }}></input>
                 <motion.button className="ml-4 bg-white text-black px-2 rounded-lg border-3 border-green-500"
-                whileTap={{ scale: 0.95 }}
+                whileTap={{ scale: 0.85 }}
                 onClick={() => {
                     
                         if(!inputValue) { return }
@@ -69,7 +69,7 @@ export function IngredientInput(props) {
                 }}
                 >Add</motion.button>
                 <motion.button className="ml-4 bg-white text-black px-2 rounded-lg border-3 border-green-400"
-                whileTap={{ scale: 0.95 }}
+                whileTap={{ scale: 0.85 }}
                 onClick={() => {
                     setIsEditing(prev => !prev)
                 }}>
